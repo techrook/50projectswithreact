34f0de6544ee95059150105a721d51aa1efccb25
techrook <mondayitohowo3@gmail.com>
2022-05-10 11:04:45 +0100

    day1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App
